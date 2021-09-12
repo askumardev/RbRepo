bc8f01c0cb17c97976489e6d88e826b39a857c91
sk534 <sk534@hotmail.com>
2021-09-12 15:22:39 +0530

    paranthesis challange updated

--- paranthesis_balance.rb
+++ paranthesis_balance.rb
@@ -17,3 +17,37 @@ end
 puts 'Enter string :'
 entered_string = gets
 puts check_balanced_parentheses?(entered_string)
+
+# require 'pry'
+# def check_balanced_parentheses?(str)
+#   stack = []
+#   str.chars.each do |token|
+#     case token
+#       when '('
+#         stack.push '('
+#       when ')'
+#         if stack.empty? || stack.pop != '('
+#           return 0
+#         end
+#       when '['
+#         stack.push '['
+#       when ']'
+#         if stack.empty? || stack.pop != '['
+#           return 0
+#         end
+#       when '{'
+#         stack.push '{'
+#       when '}'
+#         if stack.empty? || stack.pop != '{'
+#           return 0
+#         end
+#     end
+#   end
+#   if stack.empty?
+#       return 1
+#   end
+# end
+
+# puts "Enter string :"
+# entered_string = gets
+# puts check_balanced_parentheses?(entered_string)
