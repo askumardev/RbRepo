05b5c9615ecc8e784843c1f7dc9ce54c885eb637
sk534 <sk534@hotmail.com>
2021-09-13 22:37:41 +0530

    LC/palindrome

---
+++ leetcode/palindrome.rb
@@ -0,0 +1,14 @@
+def is_palindrome(x)
+  orig = x
+
+  sum = 0
+  while x > 0
+    x, rem = x.divmod(10)
+    sum = sum * 10 + rem
+    #puts sum
+  end
+  x = (sum == orig)
+  puts x
+end
+
+is_palindrome(122)
