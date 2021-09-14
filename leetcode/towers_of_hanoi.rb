6ddc818cf8e31fb8483999796f07e003ea0ccfb0
sk534 <sk534@hotmail.com>
2021-09-15 00:03:17 +0530

    towers of hanoi added

---
+++ leetcode/towers_of_hanoi.rb
@@ -0,0 +1,10 @@
+def toh(n,a,b,c)
+  if n > 0
+    toh(n-1,a,c,b) # Call function recursively to move n-1 disks from A to C using B
+    puts "Move 1 disk from #{a} to #{c}" # Move 1 disk from A to C
+    toh(n-1,b,a,c) # Call function recursively to move n-1 disks from B to C using A
+  end	
+end
+
+n = 3
+toh(n,1,2,3)
