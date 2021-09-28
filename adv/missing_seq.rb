4460599ecda6daddc44054265dd9d09c7d30e464
sk534 <sk534@hotmail.com>
2021-09-29 01:08:56 +0530

    missing seq added

---
+++ adv/missing_seq.rb
@@ -0,0 +1,8 @@
+def find_missing(sequence)
+  consecutive     = sequence.each_cons(2)
+  differences     = consecutive.map { |a,b| b - a }
+  sequence        = differences.max_by { |n| differences.count(n) }
+  missing_between = consecutive.find { |a,b| (b - a) != sequence }
+  puts missing_between.first + sequence
+end
+find_missing([2,4,6,10])
