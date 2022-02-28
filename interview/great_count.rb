529dbaca94d798d7c0a5172a18a8ec79e39692c7
askumardev <askumar.dev@outlook.com>
2022-02-28 16:25:26 +0530

    great count update

--- interview/great_count.rb
+++ interview/great_count.rb
@@ -4,5 +4,11 @@ arr = str.chars
 hsh = arr.map{|x| [x,arr.count(x)]}.to_h
 puts hsh
 puts "***********"
+#Alternate way
 hsh1 = arr.group_by(&:itself).transform_values(&:count)
 puts hsh1
+
+#o/p:
+# {"g"=>10, "r"=>13, "e"=>17, "a"=>16, "t"=>29}
+# ***********
+# {"g"=>10, "r"=>13, "e"=>17, "a"=>16, "t"=>29}
