e5df18b9704720568bc27bd50af7c786c8ce27af
askumardev <askumar.dev@outlook.com>
2022-03-12 18:48:13 +0530

    interview/seneca update

--- interview/seneca.rb
+++ interview/seneca.rb
@@ -11,5 +11,12 @@
 # end
 array = [1,2,3,4,[5,6,[7,7,8]]]
 # p arr_flatten(array)
-
 p array.to_s.gsub("[","").gsub("]","").split.map{|x| x.to_i}
+
+class Array
+  def mult2
+    map{|x| x *2 }
+  end
+end
+arr = [1,2,3,4]
+p arr.mult2
