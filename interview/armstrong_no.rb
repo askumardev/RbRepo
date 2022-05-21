97dda0524984617ebf701c378895b750e06c21c9
askumardev <askumar.dev@outlook.com>
2022-05-21 13:23:19 +0530

    armstrong update

--- interview/armstrong_no.rb
+++ interview/armstrong_no.rb
@@ -4,13 +4,13 @@ num = gets.chomp.to_i
 temp = num
 sum = 0
 
-while num!=0  #implementation of while loop
-  rem=num%10
-  num=num/10
-  sum=sum+rem*rem*rem
+while num != 0  #implementation of while loop
+  rem = num%10
+  num = num/10
+  sum = sum + rem * rem * rem
 end
 
-if(temp==sum)
+if(temp == sum)
   puts "The #{temp} is Armstrong"
 else
   puts "The #{temp} is not Armstrong"
