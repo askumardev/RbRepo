96625623266675211d8adf3503a970aa5640cc85
askumardev <askumar.dev@outlook.com>
2022-05-21 12:19:04 +0530

    interview/armstrong added

---
+++ interview/armstrong_no.rb
@@ -0,0 +1,17 @@
+puts "Enter a number::"
+num = gets.chomp.to_i
+#puts arm.to_s.split('').map(&:to_i).map { |x| x**3}.inject(:+) == arm
+temp = num
+sum = 0
+
+while num!=0  #implementation of while loop
+  rem=num%10
+  num=num/10
+  sum=sum+rem*rem*rem
+end
+
+if(temp==sum)
+  puts "The #{temp} is Armstrong"
+else
+  puts "The #{temp} is not Armstrong"
+end
