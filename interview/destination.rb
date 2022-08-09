fec97f9b1ff846c1e79680390f67139828149c35
askumardev <askumar.dev@outlook.com>
2022-08-09 13:18:33 +0530

    routes dest final

--- interview/destination.rb
+++ interview/destination.rb
@@ -5,31 +5,33 @@
 # i/p: delhi
 # o/p: delhi=>goa=>bangalore=>chennai
 
-routes = 'mumbai=>delhi=>goa=>bangalore=>chennai'.gsub('=>', ',').split(',')
-result = []
+routes = 'mumbai=>delhi=>goa=>bangalore=>chennai'.gsub!('=>', ',').split(',')
+ind = nil
 puts "#{routes.inspect}"
 puts "enter route::"
 inp = gets.chomp.to_s
 abort('no routes available/ mismatched route') if !routes.include?(inp)
-src = routes.find_index(inp)
-#dest = routes.find_index("chennai")
-dest = routes.index(routes.last)
-(src..dest).each do |x|
-  result << routes.values_at(x)
+routes.each_with_index.map { |x,i| ind = i if routes[i] == inp }
+res_arr = []
+if routes[ind] && routes[ind + 1]
+  routes.each_with_index.map { |x,i| res_arr << routes[i] if  i >= ind }
+  puts res_arr.join('=>')
+else
+  puts 'no routes available'
 end
-p result.flatten.join(",").gsub(",","=>")
 
-# routes = 'mumbai=>delhi=>goa=>bangalore=>chennai'.gsub!('=>', ',').split(',')
-# ind = nil
+#######################Alternate way below################################
+
+# routes = 'mumbai=>delhi=>goa=>bangalore=>chennai'.gsub('=>', ',').split(',')
+# result = []
 # puts "#{routes.inspect}"
 # puts "enter route::"
 # inp = gets.chomp.to_s
 # abort('no routes available/ mismatched route') if !routes.include?(inp)
-# routes.each_with_index.map { |x,i| ind = i if routes[i] == inp }
-# res_arr = []
-# if routes[ind] && routes[ind + 1]
-#   routes.each_with_index.map { |x,i| res_arr << routes[i] if  i <= ind }
-#   puts res_arr.join('=>')
-# else
-#   puts 'no routes available'
+# src = routes.find_index(inp)
+# #dest = routes.find_index("chennai")
+# dest = routes.index(routes.last)
+# (src..dest).each do |x|
+#   result << routes.values_at(x)
 # end
+# p result.flatten.join(",").gsub(",","=>")
