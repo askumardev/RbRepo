e8fbb7b8a89da47d9f1ba17a625b0923148663e0
askumardev <askumar.dev@outlook.com>
2022-09-05 14:54:17 +0530

    timezone added

---
+++ date_time/timezone.rb
@@ -0,0 +1,10 @@
+# Ruby program to get the 
+# current time zone
+
+date_time = Time.new();
+
+curretDateTime = date_time.inspect();
+
+zone  = date_time.zone();
+
+print "Current time zone is: ",zone;
