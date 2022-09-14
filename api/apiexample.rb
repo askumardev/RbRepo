6fc8ad8f7f2017e83fad619bce389ff9af0a05ae
askumardev <askumar.dev@outlook.com>
2022-09-14 20:40:38 +0530

    apiexample update

--- api/apiexample.rb
+++ api/apiexample.rb
@@ -11,3 +11,6 @@ puts res.body if res.is_a?(Net::HTTPSuccess)
 puts("*******httparty********")
 response = HTTParty.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 puts response.body if response.code == 200
+
+
+# ruby api/apiexample.rb
