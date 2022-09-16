c4f3ae83dc6a913135f3139f6487614bd53a97a6
askumardev <askumar.dev@outlook.com>
2022-09-16 10:11:19 +0530

    capgemini update

--- interview/capgem.rb
+++ interview/capgem.rb
@@ -1,3 +1,5 @@
 hsh = {'Input.txt' => 'Randy', 'Code.py' => 'Stan', 'Output.txt' => 'Randy'}
 h = Hash.new{ |hash, key| hash[key] = [] }
 p hsh.each_with_object(h) {|(k,v), h| h[v] << k}
+
+# ruby interview/capgem.rb
