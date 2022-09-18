d068497c3130082729fcf11edfc0ee23232da2d6
askumardev <askumar.dev@outlook.com>
2022-09-18 11:54:08 +0530

    70 update

--- leetcode/easy/70-climbing-stairs.rb
+++ leetcode/easy/70-climbing-stairs.rb
@@ -1,18 +1,18 @@
-# You are climbing a stair case. It takes n steps to reach to the top.
+# ruby leetcode/easy/70-climbing-stairs.rb
 
+# You are climbing a stair case. It takes n steps to reach to the top.
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 # Note: Given n will be a positive integer.
 
 # Example 1:
-
 # Input: 2
 # Output: 2
 # Explanation: There are two ways to climb to the top.
 # 1. 1 step + 1 step
 # 2. 2 steps
-# Example 2:
 
+# Example 2:
 # Input: 3
 # Output: 3
 # Explanation: There are three ways to climb to the top.
