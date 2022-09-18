31d64e5cd864cc37466a8de589c2086905e183f1
askumardev <askumar.dev@outlook.com>
2022-09-18 12:05:25 +0530

    125 update

--- leetcode/easy/125-valid-palindrome.rb
+++ leetcode/easy/125-valid-palindrome.rb
@@ -1,13 +1,14 @@
+# ruby leetcode/easy/125-valid-palindrome.rb
+
 # Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
 # Note: For the purpose of this problem, we define empty string as valid palindrome.
 
 # Example 1:
-
 # Input: "A man, a plan, a canal: Panama"
 # Output: true
-# Example 2:
 
+# Example 2:
 # Input: "race a car"
 # Output: false
 
