4d18f101384467088b128e0b5e24bde31fef3f6b
askumardev <askumar.dev@outlook.com>
2022-09-18 14:58:05 +0530

    1000 updates

--- leetcode/easy/1078-occurrences-after-bigram.rb
+++ leetcode/easy/1078-occurrences-after-bigram.rb
@@ -1,4 +1,7 @@
-# Given words first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
+# ruby leetcode/easy/1078-occurrences-after-bigram.rb
+
+# Given words first and second, consider occurrences in some text of the form "first second third", where second 
+# comes immediately after first, and third comes immediately after second.
 
 # For each such occurrence, add "third" to the answer, and return the answer.
 
--- leetcode/easy/1408-string-matching-in-an-array.rb
+++ leetcode/easy/1408-string-matching-in-an-array.rb
@@ -1,25 +1,26 @@
+# ruby leetcode/easy/1408-string-matching-in-an-array.rb
+
 # Given an array of string words. Return all strings in words which is substring of another word in any order. 
 
-# String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].
+# String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side 
+# of words[j].
 
 # Example 1:
-
 # Input: words = ["mass","as","hero","superhero"]
 # Output: ["as","hero"]
 # Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
 # ["hero","as"] is also a valid answer.
-# Example 2:
 
+# Example 2:
 # Input: words = ["leetcode","et","code"]
 # Output: ["et","code"]
 # Explanation: "et", "code" are substring of "leetcode".
-# Example 3:
 
+# Example 3:
 # Input: words = ["blue","green","bu"]
 # Output: []
  
 # Constraints:
-
 # 1 <= words.length <= 100
 # 1 <= words[i].length <= 30
 # words[i] contains only lowercase English letters.
