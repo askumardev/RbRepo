b5500b45791edad1c687caf07ec1ef2f498f67e8
askumardev <askumar.dev@outlook.com>
2022-09-18 11:50:31 +0530

    28 35 update

--- leetcode/easy/28-implement-strStr().rb
+++ leetcode/easy/28-implement-strStr().rb
@@ -1,19 +1,19 @@
+# ruby leetcode/easy/28-implement-strStr().rb
+
 # Implement strStr().
 
 # Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
 # Example 1:
-
 # Input: haystack = "hello", needle = "ll"
 # Output: 2
-# Example 2:
 
+# Example 2:
 # Input: haystack = "aaaaa", needle = "bba"
 # Output: -1
+
 # Clarification:
-
 # What should we return when needle is an empty string? This is a great question to ask during an interview.
-
 # For the purpose of this problem, we will return 0 when needle is an empty string.
 
 def str_str(haystack, needle)
@@ -25,3 +25,5 @@ def str_str(haystack, needle)
   
   -1
 end
+
+str_str('aaaaa','bba')
