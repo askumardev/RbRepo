f0c4c9be6ad2b2b1df586903742a3f8ab58f2f61
askumardev <askumar.dev@outlook.com>
2022-09-20 22:27:49 +0530

    files sphere rb added

---
+++ basics/maths/sphere_radius.rb
@@ -0,0 +1,9 @@
+# ruby basics/maths/sphere_radius.rb
+
+r = 5.0
+v = 0.0
+pi = 3.141592653
+print "Input the radius of the circle: "
+r = gets.to_f
+v = (4/3)* (pi)* (r**3)
+puts "The volume of the sphere is : #{v}."
