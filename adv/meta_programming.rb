96eec011ee8918f3505a497b6b0beb7be91be634
askumardev <askumar.dev@outlook.com>
2023-02-19 11:59:32 +0530

    meta programming notes

---
+++ adv/meta_programming.rb
@@ -0,0 +1,13 @@
+# Metaprogramming in Ruby refers to the ability to write code that can generate other code or modify the behavior of a program at runtime. Ruby's flexibility allows for powerful metaprogramming techniques, and it is a key feature of the language.
+
+# Some examples of metaprogramming techniques in Ruby include:
+
+# Dynamically defining classes and methods: Ruby allows you to define new classes and methods at runtime using the Class.new and define_method methods. This is useful when you need to generate classes or methods on the fly, such as when building a domain-specific language.
+
+# Using method_missing: When a method is called that does not exist, Ruby will call the method_missing method, which you can override to implement custom behavior. This is useful for implementing dynamic dispatch, where the behavior of a method can vary depending on the arguments passed to it.
+
+# Defining class macros: Ruby allows you to define macros that can generate code, such as attr_accessor, which generates getters and setters for instance variables. This is useful when you want to provide a concise way to define common functionality.
+
+# Using reflection: Ruby provides reflection APIs that allow you to introspect the code at runtime, such as the Object#methods method, which returns a list of all the methods defined on an object. This is useful when you need to build tools that analyze or manipulate code.
+
+# Metaprogramming in Ruby can be very powerful, but it can also make code harder to read and understand, particularly if it is overused or used inappropriately. It is important to use metaprogramming judiciously and to document it clearly so that other developers can understand the code.
