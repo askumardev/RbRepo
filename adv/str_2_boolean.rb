92624abbc28a99d6722726d883dcef3436c7bfcf
askumardev <askumar.dev@outlook.com>
2023-02-20 21:38:52 +0530

    str to bool another ex

--- adv/str_2_boolean.rb
+++ adv/str_2_boolean.rb
@@ -8,3 +8,18 @@ boolean = var.to_s.eql?('true') ? true : false
 
 #robocop suggested format
 "true".to_s.casecmp('true').zero?
+###################################################
+class String
+  def to_b
+    case self.downcase.strip
+    when 'true', 'yes', 'on', 't', '1', 'y', '=='
+      return true
+    when 'nil', 'null'
+      return nil
+    else
+      return false
+    end
+  end
+end
+p "true".to_b
+#######################################
