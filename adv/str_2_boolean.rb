4b6d1f63d93b2f814f47473107667c2aec106306
askumardev <askumar.dev@outlook.com>
2023-02-20 21:41:53 +0530

    str to bool ex3

--- adv/str_2_boolean.rb
+++ adv/str_2_boolean.rb
@@ -23,3 +23,11 @@ class String
 end
 p "true".to_b
 #######################################
+class String
+  def to_bool
+    return true   if self == true   || self =~ (/(true|t|yes|y|1)$/i)
+    return false  if self == false  || self.blank? || self =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+  end
+end
+p "true".to_bool
