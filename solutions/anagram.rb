7e18a8fce9cc201bdfe856f452b3929e606c47c4
askumardev <askumar.dev@outlook.com>
2023-12-25 00:32:45 +0530

    anagram update

--- solutions/anagram.rb
+++ solutions/anagram.rb
@@ -1,30 +1,31 @@
 # ruby solutions/anagram.rb
+require 'pry'
 
 def count_anagrams(parent, child)
   # Remove spaces and convert to lowercase for case-insensitive comparison
-  clean_parent = parent.downcase.gsub(/\s+/, '')
-  clean_child = child.downcase.gsub(/\s+/, '')
+  parent = parent.downcase.gsub(/\s+/, '')
+  child = child.downcase.gsub(/\s+/, '')
 
   # Initialize a counter for the number of occurrences
   count = 0
-
   # Iterate through substrings of the parent string
-  clean_parent.chars.each_cons(clean_child.length) do |substring|
+  parent.chars.each_cons(child.length) do |substring|
     # Check if the substring is an anagram of the child string
-    count += 1 if substring.sort == clean_child.chars.sort
+    puts(substring.sort)
+    count += 1 if substring.sort == child.chars.sort
   end
 
   count
 end
 
 # Example usage:
-parent_string = "An example string that contains an anagram of silent."
-child_string = "listen"
+# parent_string = "An example string that contains an anagram of silent."
+# child_string = "listen"
 
-occurrences = count_anagrams(parent_string, child_string)
-puts "The child string or its anagrams appear #{occurrences} times in the parent string."
+# occurrences = count_anagrams(parent_string, child_string)
+# puts "The child string or its anagrams appear #{occurrences} times in the parent string."
 
-parent_string='AdnBndAndBdaBn'
-child_string='dAn'
-occurrences = count_anagrams(parent_string, child_string)
+parent = 'AdnBndAndBdaBn'
+child = 'dAn'
+occurrences = count_anagrams(parent, child)
 puts "The child string or its anagrams appear #{occurrences} times in the parent string."
