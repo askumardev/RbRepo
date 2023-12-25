a3c534724bb38e78cdfde299914c6f0247bb18ba
askumardev <askumar.dev@outlook.com>
2023-12-25 18:37:14 +0530

    frequent update

--- solutions/frequent.rb
+++ solutions/frequent.rb
@@ -1,8 +1,11 @@
+#ruby solutions\frequent.rb
+
 inp = "346555578123999021344"
 puts inp.chars.chunk{|e| e}.map{
   |e| e[1].join if e[1].size >1 
 }.compact.inspect
 
+puts("--------------------------------------------------")
 # Find Most Frequently Occurring Element in an Array With Ruby
 arr = [1, 2, 3, 4, 5, 5, 5, 6, 6,6,6,6, 7, 7, 7, 7]
 most_frequent = arr.group_by(&:itself).max_by { |_, v| v.size }.first
