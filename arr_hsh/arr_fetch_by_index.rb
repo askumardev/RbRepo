9924422763f8014af676eb836069f7d357c735d6
askumardev <askumar.dev@outlook.com>
2023-12-30 13:21:35 +0530

    arr hsh programs update 1

--- arr_hsh/arr_fetch_by_index.rb
+++ arr_hsh/arr_fetch_by_index.rb
@@ -1,14 +1,12 @@
 # Ruby program to fetch element from 
 # array based on index
 
-city = ["AGRA", "DELHI", "MUMBAI", "GWALIOR"];
+city = ["AGRA", "DELHI", "MUMBAI", "GWALIOR"]
 
-print "Enter index: ";
-index = gets.chomp.to_i;  
-
-item = city.fetch(index);
-
-print "City is: ",item,"\n";
+print "Enter index: "
+index = gets.chomp.to_i
+item = city.fetch(index)
+print "City is: ",item,"\n"
 
 
 # ruby arr_hsh/arr_fetch_by_index.rb
--- arr_hsh/arr_fill.rb
+++ arr_hsh/arr_fill.rb
@@ -1,14 +1,14 @@
-# Ruby program to fill an array 
-# with specific element
+# Ruby program to fill an array with specific element
 
 city = ["AGRA", "DELHI", "MUMBAI", "GWALIOR"];
 
-print "Enter string: ";
-str = gets.chomp;  
+print "Enter string: "
+str = gets.chomp
+city.fill(str)
 
-city.fill(str);
-
-print "Cities are: \n",city,"\n";
-
+print "Cities are: \n",city,"\n"
 
 # ruby arr_hsh/arr_fill.rb
+# Enter string: abc
+# Cities are: 
+# ["abc", "abc", "abc", "abc"]
