e6648e0f73fc304056b1e00cb2a0b2bdff3dd4a2
askumardev <askumar.dev@outlook.com>
2023-12-31 22:03:25 +0530

    ||= operator update

--- basics/or_equal_operator.rb
+++ basics/or_equal_operator.rb
@@ -3,17 +3,13 @@
 # In Ruby, the "||=" operator is used to assign a value to a variable only if that variable
 # is currently nil or false. Here's an example:
 
+x = nil # Initialize variable x to nil
 
-# Initialize variable x to nil
-x = nil
-
-# Use ||= to assign a value only if x is nil
-x ||= 10
+x ||= 10 # Use ||= to assign a value only if x is nil
 
 puts x  # Output: 10
 
-# If x already has a value, ||= won't change it
-x ||= 20
+x ||= # If x already has a value, ||= won't change it
 
 puts x  # Output: 10 (unchanged)
 
