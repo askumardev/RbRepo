90dc97afacecba0e7f84e442ba035ac99732a464
askumardev <askumar.dev@outlook.com>
2024-01-04 16:51:54 +0530

    words swap

---
+++ arr_hsh/str_rev.rb
@@ -0,0 +1,25 @@
+str = "BLUE SKY"
+puts "------Simplest way by using split, reverse and join methods-----"
+str_rev = str.split.reverse.join(' ')
+p str_rev
+
+puts "------Alternate way-------"
+words = str.split
+p words
+words[0], words[1] = words[1], words[0]
+p words.join(' ')
+
+puts "------Alternate way without using reverse-------"
+words = str.split
+p words
+reversed_string = ""
+
+(words.length - 1).downto(0) do |i|
+  reversed_string += words[i] + " "
+end
+p reversed_string
+reversed_string.rstrip!  # Remove the trailing space
+
+puts reversed_string
+
+
