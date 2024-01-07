6029ebae67a2100a5f7de4b5a994a37d6cf17cb2
askumardev <askumar.dev@outlook.com>
2024-01-07 15:55:58 +0530

    design patterns

---
+++ design_patterns/factory_dp.rb
@@ -0,0 +1,21 @@
+# class Endpoint
+#   def home(params)
+#     if params[:user_type] == "admin"
+#       Admin.new
+#     elsif params[:user_type] == "member"
+#       Member.new
+#     else
+#       Guest.new
+#     end
+#   end
+
+#   def contacts(params)
+#     if params[:user_type] == "admin"
+#       Admin.new
+#     elsif params[:user_type] == "member"
+#       Member.new
+#     else
+#       Guest.new
+#     end
+#   end
+# end
