dff25a8d85d6e3e93327f02996d5e72440c1fcae
askumardev <askumar.dev@outlook.com>
2024-01-11 16:32:36 +0530

    digits addition

--- basics/maths/sum_of_digits.rb
+++ basics/maths/sum_of_digits.rb
@@ -19,3 +19,19 @@ puts 'Sum of digits of 1234 is ' + digits_sum(1234).to_s
 # Sum of digits of 1234 is 10
 puts 'Sum of digits of 9251321 is ' + digits_sum(9_251_321).to_s
 # Sum of digits of 9251321 is 23
+
+p("--------------Alternate way---------")
+
+num = 537
+sum = 0
+
+while num > 0
+  sum = sum + (num%10)
+  num = num/10
+end
+if num == 0 && sum > 0
+  num = sum
+  sum = 0
+end
+
+p num
