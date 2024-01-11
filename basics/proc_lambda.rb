6b6497b5b2e365bb82645b88d6f7b09b343e7522
askumardev <askumar.dev@outlook.com>
2024-01-10 23:18:49 -0800

    proc lambda update

--- basics/proc_lambda.rb
+++ basics/proc_lambda.rb
@@ -79,3 +79,16 @@ def lambda_method
 end
 
 lambda_method
+
+def method_proc
+  thing  = Proc.new { return 1}
+  thing.call
+  return 2
+end
+def method_lambda
+  thing  = lambda { return 1}
+  thing.call
+  return 2
+end
+puts method_proc # => 1
+puts method_lambda # => 2
