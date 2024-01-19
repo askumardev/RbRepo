00e1ce313d58010dcec95033f556feb1fededde1
askumardev <askumar.dev@outlook.com>
2024-01-19 21:02:28 +0530

    dpworld interview

--- Gemfile
+++ Gemfile
@@ -5,5 +5,6 @@ gem 'rubocop'
 gem 'rspec'
 gem 'awesome_print', :require => 'ap'
 gem 'dotenv-rails', require: 'dotenv/rails-now'
+gem 'httparty'
 
 gem 'benchmark'
---
+++ interview/dpworld.rb
@@ -0,0 +1,28 @@
+# ruby interview/dpworld.rb
+
+require 'httparty'
+require 'json'
+require 'pry'
+
+url = "https://demo2459056.mockable.io/url"
+json = HTTParty.get(url)
+a = JSON.parse(json.body)
+selected_values = a["data"].map { |x| { "city" => x["city"], "cost" =>x["estimated_cost"] } }#.group_by{|x| x["city"], avg(x["cost"])}
+#p selected_values
+grouped_data = selected_values.group_by { |entry| entry["city"] }
+#p grouped_data
+
+# Calculate average cost for each city and format the result
+average_costs = grouped_data.map do |city, entries|
+  avg_cost = entries.sum { |entry| entry["cost"] } / entries.length.to_f
+  {"city" => city, "avg" => avg_cost}
+end
+puts average_costs
+p("-------------2nd round---------")
+arr = [0,-1,2,-3,1]
+target = 2 
+
+arr.permutation(2) do |x|
+#[[0,2],[2,0]]
+ p x.first if x.inject(:+) == target 
+end
