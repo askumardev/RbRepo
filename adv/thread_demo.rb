c55835b8024d1fe6d2db5da76fbef1aa7913afd8
askumardev <askumar.dev@outlook.com>
2024-01-24 05:25:32 +0530

    thread demo update

--- adv/thread_demo.rb
+++ adv/thread_demo.rb
@@ -1,13 +1,20 @@
+# ruby adv/thread_demo.rb
+
 counter = 0
 
-10.times.map do
-  Thread.new do
-    1000.times do
-      p counter
-      counter += 1
-    end
-  end
-end.each(&:join)
+# Creating and running threads
+thread1 = Thread.new do
+  5.times { |i| puts "Thread 1: #{i}" }
+end
+
+thread2 = Thread.new do
+  5.times { |i| puts "Thread 2: #{i}" }
+end
+
+# Wait for threads to finish
+thread1.join
+thread2.join
+
 puts("---------------mutex example below-----------")
 mutex = Mutex.new
 shared_variable = 0
@@ -28,4 +35,13 @@ end
 
 thread1.join
 thread2.join
+
+# 10.times.map do
+#   Thread.new do
+#     1000.times do
+#       p counter
+#       counter += 1
+#     end
+#   end
+# end.each(&:join)
 
