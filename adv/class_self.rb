014e1f7d9ec9a7c49cb726766537cbca6c61da82
askumardev <askumar.dev@outlook.com>
2024-01-24 05:17:37 +0530

    class self update

--- adv/class_self.rb
+++ adv/class_self.rb
@@ -1,11 +1,26 @@
 class MyClass
+  puts self       
+  puts self.class
+  p "--------------------------"
   def self.class_method
     puts "This is a class method"
   end
 
+  obj = Object.new
+  class << obj
+    def helloooo
+      puts "Helloooo!"
+    end
+  end
+  obj.helloooo
+
   class << self
     def another_class_method
       puts "This is another class method"
+    end
+
+    def hello
+      p "hello..."
     end
   end
 end
@@ -13,7 +28,8 @@ end
 # Calling class methods
 MyClass.class_method      # Output: This is a class method
 MyClass.another_class_method  # Output: This is another class method
-
+MyClass.hello
+puts self, self.class
 
 # ruby adv/class_self.rb
   
