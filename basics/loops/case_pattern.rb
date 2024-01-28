63c4597b1b965b7a0472c75474f4f1e2d89433d0
askumardev <askumar.dev@outlook.com>
2024-01-28 09:55:16 +0530

    case pettern update

--- basics/loops/case_pattern.rb
+++ basics/loops/case_pattern.rb
@@ -1,23 +1,23 @@
 users = [
- {name: "Alice", age:30 },
- {name: "Bob", age:18 },
- {name: "Clive", age:35 },
- {name: "David", age:16 },
- {name: "Alice", age:20 },
- {name: "Eve", age:40 }
+  { name: "Alice", age: 30 },
+  { name: "Bob", age: 18 },
+  { name: "Clive", age: 35 },
+  { name: "David", age: 16 },
+  { name: "Flora", age: 20 },
+  { name: "Eve", age: 40 }
 ]
 
 users.each do |user|
- case user
- in { name:name, age: 0..17 }
-  puts "#{name} is teen"
- in { name:name, age: 18..25 }
-  puts "#{name} is young"
- in { name:name, age: 26..35 }
-  puts "#{name} is in mid 30s"
- else
-  puts "#{name} is not categorized"
- end
+  case user
+  in { name:name, age: 0..17 }
+    puts "#{name} is teen"
+  in { name:name, age: 18..25 }
+    puts "#{name} is young"
+  in { name:name, age: 26..35 }
+    puts "#{name} is in mid 30s"
+  else
+    puts "#{name} is not categorized"
+  end
 end
 
 # ruby basics/loops/case_pattern.rb
