09d0857c0a45f3bb4326722cfd815857fd854323
askumardev <askumar.dev@outlook.com>
2024-06-05 23:19:17 -0700

    arr uniq hash

--- interview/arr_uniq_hash.rb
+++ interview/arr_uniq_hash.rb
@@ -1,9 +1,14 @@
 def remove_duplicates(array)
+  seen = {}
   result = []
-
+  
   array.each do |element|
-    result << element unless result.include?(element)
+    unless seen[element]
+      result << element
+      seen[element] = true
+    end
   end
+  
   result
 end
 
@@ -12,4 +17,4 @@ arr = [1, 2, 2, 3, 4, 4, 5]
 puts remove_duplicates(arr).inspect
 # Output: [1, 2, 3, 4, 5]
 
-# ruby interview/arr_uniq_array.rb
+# ruby interview/arr_uniq_hash.rb
