5b0e348842d3ed78c46f14fb56800a36c5db8713
askumardev <askumar.dev@outlook.com>
2024-06-05 23:15:26 -0700

    arrau uniq array added

---
+++ interview/arr_uniq_array.rb
@@ -0,0 +1,13 @@
+def remove_duplicates(array)
+  result = []
+	
+  array.each do |element|
+    result << element unless result.include?(element)
+  end    
+  result
+end
+
+# Example usage
+arr = [1, 2, 2, 3, 4, 4, 5]
+puts remove_duplicates(arr).inspect
+# Output: [1, 2, 3, 4, 5]
