f5962cf390596b105c4b541c3bf8a77825b9f574
askumardev <askumar.dev@outlook.com>
2024-06-10 10:45:09 -0700

    loops update

--- basics/loops/upto_downto.rb
+++ basics/loops/upto_downto.rb
@@ -5,3 +5,12 @@ end
 5.downto(1) do |number|
   puts number
 end
+
+puts "***upto "
+5.upto(10) do |i|
+  puts "i = #{i}"
+end
+puts "***upto alternate using blocks"
+5.upto(10) { |i| puts i}
+
+# ruby basics/loops/upto_downto.rb
--- basics/loops/while.rb
+++ basics/loops/while.rb
@@ -3,7 +3,6 @@
 # while loop is executes repeatedly as long as condition is true
 
 x = 4
-
 while x >= 1   # do keyword is optional
   puts "Satish"
   x = x-1
