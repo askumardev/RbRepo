0c31ffc0bf4806c97803d0073e043da4c7eeaea3
askumardev <askumar.dev@outlook.com>
2024-06-10 21:54:29 -0700

    great alternate ways

--- interview/great_count.rb
+++ interview/great_count.rb
@@ -1,11 +1,23 @@
 str = "ggggggggggrrrrrrrrrrrrreeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaattttttttttttttttttttttttttttt"
+puts "*****using map******"
 arr = str.chars
-
 hsh = arr.map{|x| [x,arr.count(x)]}.to_h
 puts hsh
-puts "***********"
 
+puts "*****using tally******"
+hsh = arr.tally
+puts hsh
 
+puts "--------------using Hash--------"
+hsh = Hash.new(0)
+arr.each { |x| hsh[x] += 1 }
+puts hsh
+
+puts "--------------using inject--------"
+hsh = arr.inject(Hash.new(0)) { |h, x| h[x] += 1; h }
+puts hsh
+
+puts "****using group_by*******"
 #Alternate way
 hsh1 = arr.group_by(&:itself).transform_values(&:count)
 puts hsh1
