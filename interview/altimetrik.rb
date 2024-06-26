527680ee6d086bc4b7f240e8cc9ff1a92e445424
askumardev <askumar.dev@outlook.com>
2024-06-26 10:44:47 +0530

    altimetrik update

--- interview/altimetrik.rb
+++ interview/altimetrik.rb
@@ -22,3 +22,39 @@ end
 p is_palin(-11) # should return false
 p is_palin(1234) # should return false
 p is_palin(1221) # should return true
+#---------------------
+class MyController < ApplicationController
+    def options
+        options = {}
+        available_option_keys = [:first_option, :second_option, :third_option]
+        all_keys = params.keys.map(&:to_sym) #[:first,:second]
+        set_option_keys = all_keys & available_option_keys #[:first,:second, :first_option, :second_option, :third_option]
+        set_option_keys.each do |key|
+            options[key] = params[key]
+        end
+        #options = set_option_keys.map{|x| options[x] = params[x]}
+        puts options
+    end
+end
+
+
+RSpec.define coontroller do
+ describe "GET options" do
+      before do
+      options ={}
+       available_option_keys = [:first_option, :second_option, :third_option]
+      end
+      let(:all_keys) = params.keys.map(&:to_sym) #[:first,:second]
+
+      it "should fetch set all options" do
+       set_option_keys = all_keys & available_option_keys
+
+       expect(options).should_not be empty
+      end
+
+  end
+end
+
+
+hsh = {"a": 1, "b":2}
+p hsh.to_json
