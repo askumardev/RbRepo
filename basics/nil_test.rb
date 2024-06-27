098672c3cb8d4c6edbe50bbeb63d6cb462ab58ac
askumardev <askumar.dev@outlook.com>
2024-06-27 12:37:03 +0530

    nil test update

--- basics/nil_test.rb
+++ basics/nil_test.rb
@@ -1,3 +1,33 @@
 p "".nil?
 p "".empty?
 p "".blank?
+
+students = ["Jeery", "Tom", "Eddie"]
+
+puts students[0] #this will print student at index 0
+
+puts students[4] #nil  #this will output nothing since index doesnot exist
+
+puts students[4].class #this will output the class that belongs to nil
+
+puts students[0] == students[1] #false ->this will output false since both names does not match
+
+puts (students[0] == students[1]).class #this will output the class that belongs to false
+
+
+#special case
+#nil treated as false
+Jeery = nil
+if Jeery
+   puts "Jeery is present"
+else 
+   puts "Jeery is absent"
+end
+
+#false treated as false
+Tom = false
+if Tom
+   puts "Tom is present"
+else 
+   puts "Tom is absent"
+end
