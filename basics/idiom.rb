831f8e95509368963bb6b757e91a62d590867461
askumardev <askumar.dev@outlook.com>
2024-07-01 11:57:52 +0530

    idiom file update

--- basics/idiom.rb
+++ basics/idiom.rb
@@ -1,3 +1,11 @@
+# In Ruby, the ||= operator is a common idiom used to assign a value to a variable if the variable is either 
+# nil or false. Essentially, it is a conditional assignment operator. It is a shorthand for:
+
+
+# variable = variable || value
+# In other words, if variable is nil or false, it gets assigned the value on the right-hand side. 
+# If variable already has a truthy value (anything other than nil or false), it remains unchanged.
+
 a = nil
 a ||= 10
 puts a # => 10
