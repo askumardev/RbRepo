1deb03d72538c11c2a68a9d2be8d42a5f6b921d7
askumardev <askumar.dev@outlook.com>
2024-07-19 14:41:47 +0530

    addition update

--- hackerRank/addition.rb
+++ hackerRank/addition.rb
@@ -10,10 +10,11 @@ def solveMeFirst (a, b)
   if a >= 1 && b <= 1000
     a + b
   else
-    puts "a is either zero or positive"
+    puts "a is either zero/negative or b is greater than 1000"
   end
 end
 
 puts solveMeFirst(7,3)
 puts solveMeFirst(0,3)
 puts solveMeFirst(-7,3)
+puts solveMeFirst(7,3000)
