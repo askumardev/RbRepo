552ada0fbc7e6731c98bfbab04e6b8a24002d8aa
askumardev <askumar.dev@outlook.com>
2024-07-22 09:40:30 +0530

    arr uniq using hash and set

--- arr_hsh/arr_uniq_hash.rb
+++ arr_hsh/arr_uniq_hash.rb
@@ -1,25 +1,41 @@
-def remove_duplicates(array)
-  seen = {}
-  result = []
-  
-  array.each do |element|
-    unless seen[element]
-      result << element
-      seen[element] = true
-    end
+# ruby arr_hsh/arr_uniq_hash.rb
+p "------------using hash------------------------"
+def remove_duplicates(arr)
+  hsh = {}
+  arr.each do |x|
+    hsh[x] = x
   end
-  
-  result
+  hsh.values
 end
 
-# Example usage
 arr = [1, 2, 2, 3, 4, 4, 5]
 puts remove_duplicates(arr).inspect
+p "-------------using Set---------------------"
+p Set.new(arr).to_a
+
+
+# def remove_duplicates(array)
+#   seen = {}
+#   result = []
+
+#   array.each do |element|
+#     unless seen[element]
+#       result << element
+#       seen[element] = true
+#     end
+#   end
+
+#   result
+# end
+
+# # Example usage
+# arr = [1, 2, 2, 3, 4, 4, 5]
+# puts remove_duplicates(arr).inspect
 # Output: [1, 2, 3, 4, 5]
 
-# ruby interview/arr_uniq_hash.rb
+# ruby arr_hsh/arr_uniq_hash.rb
 
-# Each of these methods will remove duplicates from an array without using the uniq method. 
-# The hash-based approach (Method 1) and the Set-based approach (Method 3) generally offer better performance for large arrays 
-# compared to the include? method (Method 2), 
+# Each of these methods will remove duplicates from an array without using the uniq method.
+# The hash-based approach (Method 1) and the Set-based approach (Method 3) generally offer better performance for large arrays
+# compared to the include? method (Method 2),
 # which has a time complexity of O(n^2) due to repeated searches in the result array.
