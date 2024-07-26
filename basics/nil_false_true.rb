32547c32614cfb5e360d0399dbe5d1786f807280
askumardev <askumar.dev@outlook.com>
2024-07-26 16:59:24 +0530

    nil false true update

--- basics/nil_false_true.rb
+++ basics/nil_false_true.rb
@@ -1,5 +1,20 @@
 # ruby basics/nil_false_true.rb
 
+# In Ruby, nil, false, and true are special objects that represent the concepts of 
+# "nothing," "falseness," and "truth," respectively.
+
+# nil is an object of the NilClass and represents the absence of a value or nothingness. 
+# It is commonly used to indicate that a variable, method, or expression has no value. In Ruby, nil is considered "falsy."
+
+# false is an object of the FalseClass and represents a boolean false value. 
+# When a condition evaluates to false, it means the condition is not met. In Ruby, false is considered "falsy."
+
+# true is an object of the TrueClass and represents a boolean true value. 
+# When a condition evaluates to true, it means the condition is met. In Ruby, true is considered "truthy."
+
+# All values in Ruby, other than nil and false, are considered "truthy." 
+# This means that when used in a condition, they will be treated as true. 
+
 def check_value(value)
   if value
     puts "The value '#{value}' is considered truthy."
