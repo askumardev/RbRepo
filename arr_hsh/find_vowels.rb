81513832ff2dac852c07a2b3d48087f5de2d59d6
askumardev <askumar.dev@outlook.com>
2024-08-01 13:25:22 +0530

    find vowels

--- arr_hsh/find_vowels.rb
+++ arr_hsh/find_vowels.rb
@@ -2,8 +2,10 @@
 
 str = "ggggrrreeeeeeaaaaaaatttttttttttttt"
 vowel_arr = %w(a e i o u)
-
 arr = str.chars
 hsh = arr.map{|x| [x,arr.count(x)]}.to_h
 p hsh
+p "-------vowels are-------------"
 p hsh.keys.select{|x| vowel_arr.include?(x)}
+p "-------vowels count-------------"
+p hsh.select { |k, v| vowel_arr.include?(k) }
