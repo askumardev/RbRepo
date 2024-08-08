f6c3c2c320441b362f34d895425b3732588305d8
askumardev <askumar.dev@outlook.com>
2024-08-08 11:56:32 +0530

    vowel_consonents

---
+++ arr_hsh/vovel_consonents.rb
@@ -0,0 +1,15 @@
+str = "satish Kumar"
+arr = str.chars
+vowel_arr = %w(a e i o u)
+vowels = []
+consonents = []
+p arr
+arr.each do |x|
+  vowels << x if vowel_arr.include?(x)
+  consonents << x unless vowel_arr.include?(x)
+end
+p vowels
+p vowels.count
+consonents = consonents.select { |element| element != " " }
+p consonents
+p consonents.count
