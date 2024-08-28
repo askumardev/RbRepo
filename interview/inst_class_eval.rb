a0f9a4f5c752bc3add2b6ea3491f28186c3df034
askumardev <askumar.dev@outlook.com>
2024-08-28 15:38:13 +0530

    inst eval class eval demo

--- interview/inst_class_eval.rb
+++ interview/inst_class_eval.rb
@@ -1,28 +1,42 @@
+# ruby interview/inst_class_eval.rb
+
 class Person
-  def initialize(name)
-    @name = name
-  end
 end
-
-person1 = Person.new("bob")
-person2 = Person.new("jane")
-
-person1.instance_eval do
-  def name
-    @name.capitalize
-  end
-end
-
-p person1.name
-# p person2.name   # undefined method `name' for #<Person:0x000056414a80f048 @name="jane"> (NoMethodError)
 
 Person.class_eval do
-  def name
-    @name.capitalize
+  def say_hello
+   "Hello!"
   end
 end
 
-p person1.name
-p person2.name
-person3 = Person.new("Susan")
-p person3.name
+jimmy = Person.new
+p jimmy.say_hello # "Hello!"
+# In this example, class_eval allows us to define a method within the Person class outside of its original definition and without reopening the class with the standard syntax. This could be useful when the class you want to add this method to is not known until runtime.
+
+
+Person.instance_eval do
+  def human?
+    true
+  end
+end
+
+p Person.human? # true
+
+# This example of instance_eval is similar, but evaluates the code in the context of an instance instead of a class.
+# This is confusing at first, because in these examples class_eval creates instance methods and instance_eval creates class methods.
+# There is reason behind the madness, however.
+
+# class_eval is a method of the Module class, meaning that the receiver will be a module or a class.
+# The block you pass to class_eval is evaluated in the context of that class.
+# Defining a method with the standard def keyword within a class defines an instance method, and that's exactly what happens here.
+
+# instance_eval, on the other hand, is a method of the Object class, meaning that the receiver will be an object.
+# The block you pass to instance_eval is evaluated in the context of that object.
+# That means that Person.instance_eval is evaluated in the context of the Person object.
+# Remember that a class name is simply a constant which points to an instance of the class Class.
+# Because of this fact, defining a method in the context of Class instance referenced by Person creates a class method for Person class.
+
+# It may be difficult to wrap your mind around that if you're not familiar with the Ruby object model,
+# but it's still easy to remember how these methods behave with a simple mnemonic device: when called on a class name constant,
+# these two methods will allow you to create methods of the opposite type from their names.
+# MyClass.class_eval will create instance methods and MyClass.instance_eval will create class methods.
