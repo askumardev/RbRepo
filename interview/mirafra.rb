4b800ad10cee24490f6b8eeb256050a69d4029f7
askumardev <askumar.dev@outlook.com>
2024-09-12 12:53:07 +0530

    dup clone update

--- interview/mirafra.rb
+++ interview/mirafra.rb
@@ -11,6 +11,8 @@ def obj.hello
 end
 
 p obj.hello
-
 # p obj.dup.hello     #undefined method `hello' for an instance of Object (NoMethodError)
 p obj.clone.hello
+
+# clone copies any singleton methods that were defined for a specific object.
+# dup does not copy singleton methods, meaning the new object won't have access to them.
