6885f468a52a7fe968fd5464cbe298a0506c851f
askumardev <askumar.dev@outlook.com>
2026-03-10 22:43:30 +0530

    countdown timer

--- adv/countdown_timer/server.rb
+++ adv/countdown_timer/server.rb
@@ -16,3 +16,20 @@ end
 trap('INT') { server.shutdown }
 
 server.start
+
+# ruby adv/countdown_timer/server.rb
+
+require 'webrick'
+
+server = WEBrick::HTTPServer.new(Port: 4567)
+
+html_path = File.join(__dir__, "timer.html")
+
+server.mount_proc '/' do |req, res|
+  res['Content-Type'] = 'text/html'
+  res.body = File.read(html_path)
+end
+
+trap('INT') { server.shutdown }
+
+server.start
