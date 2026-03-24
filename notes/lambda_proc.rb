ef45679f1fd7523677a15a31d282fc050be4e60b
askumardev <askumar.dev@outlook.com>
2026-03-24 19:52:42 +0530

    idiom

--- notes/lambda_proc.rb
+++ notes/lambda_proc.rb
@@ -2,19 +2,16 @@ In Ruby, lambda is a type of anonymous function that can be created using the la
 
 Here's an example of creating a lambda in Ruby:
 
-python
-Copy code
+
 my_lambda = lambda { |x| x * 2 }
 This creates a lambda that takes one argument and doubles it. The lambda can then be called using the call method or the shorthand [] syntax:
 
-python
-Copy code
+
 my_lambda.call(3)   # returns 6
 my_lambda[4]        # returns 8
 One important difference between lambdas and Procs in Ruby is that lambdas enforce arity, which means they will raise an error if the wrong number of arguments is passed to them. Procs, on the other hand, will silently ignore extra arguments or fill in missing ones with nil.
 
-cpp
-Copy code
+
 my_proc = Proc.new { |x| x * 2 }
 
 my_proc.call(3, 4, 5)   # returns 6, ignoring extra arguments
@@ -23,26 +20,23 @@ my_lambda.call(3, 4, 5) # raises ArgumentError: wrong number of arguments (given
 Overall, lambdas are a useful tool for creating self-contained blocks of code that can be passed around as objects and called at a later time.
 
 
-####################################################### 
+#######################################################
 
 In Ruby, a proc is an object that represents a block of code that can be saved and executed later. It's similar to a lambda, which is another way of defining a block of code in Ruby.
 
 A proc can be created using the Proc.new method or by using the proc method. Here's an example:
 
-go
-Copy code
+
 my_proc = Proc.new { puts "Hello, world!" }
 This creates a new proc object that contains the block of code that simply outputs "Hello, world!" to the console.
 
 Once a proc is created, it can be called using the call method, like this:
 
-c
-Copy code
+
 my_proc.call  # outputs "Hello, world!"
 Procs can also take arguments, like this:
 
-c
-Copy code
+
 my_proc = Proc.new { |name| puts "Hello, #{name}!" }
 my_proc.call("John")  # outputs "Hello, John!"
 In this example, the proc takes one argument, name, and outputs a personalized greeting using that argument.
