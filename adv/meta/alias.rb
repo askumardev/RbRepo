c1d2aa18805488fd9fc4bd722b94b5ed6dc720eb
askumardev <askumar.dev@outlook.com>
2026-03-31 23:11:33 +0530

    meta updates

---
+++ adv/meta/alias.rb
@@ -0,0 +1,15 @@
+class Hello
+  def greet
+    "Hello"
+  end
+
+  alias_method :original_greet, :greet
+
+  def greet
+    original_greet + " Satish"
+  end
+end
+
+puts Hello.new.greet  # => Hello Satish
+
+# ruby adv/meta/alias.rb
