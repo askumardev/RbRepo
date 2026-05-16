74e600668c337ac0b223c716de90a8ec780f291e
askumardev <askumar.dev@outlook.com>
2026-05-16 13:16:42 +0530

    2 sum

--- leetcode/easy/1_two_sum.rb
+++ leetcode/easy/1_two_sum.rb
@@ -1,43 +1,66 @@
-#We have an array with elements [2,5,11,7,-2] and the target is 9. 
+#We have an array with elements [2,5,11,7,-2] and the target is 9.
 # Get all pairs that add up to the target
 # the output should be [[0, 3], [2, 4]], as both pairs (2, 7) and (11, -2) add up to 9.
 
 # ruby leetcode/easy/1_two_sum.rb
-require 'pry'
-def two_sum_indices(nums, target)
-  num_indices = {}
-  result = []
+def two_sum(nums, target)
+  hash = {}
 
   nums.each_with_index do |num, index|
+    p "------iteration #{index}--------"
     complement = target - num
-    if num_indices.key?(complement)
-      result << [num_indices[complement], index]
+    p "complement = #{complement}"
+
+    if hash.key?(complement)
+      return [hash[complement], index]
     end
-    num_indices[num] = index
+    p hash
+    hash[num] = index
   end
-  return result
 end
 
-# Example usage
-nums = [2, 5, 11, 7, -2]
+# Example
+nums = [2, 11, 15, 7]
 target = 9
-result = two_sum_indices(nums, target)
 
-if result.empty?
-  puts "No such pairs found."
-else
-  puts "Indices of the pairs: #{result}"
-end
+puts two_sum(nums, target).inspect
+
+
+# require 'pry'
+# def two_sum_indices(nums, target)
+#   num_indices = {}
+#   result = []
+
+#   nums.each_with_index do |num, index|
+#     complement = target - num
+#     if num_indices.key?(complement)
+#       result << [num_indices[complement], index]
+#     end
+#     num_indices[num] = index
+#   end
+#   return result
+# end
+
+# # Example usage
+# nums = [2, 5, 11, 7, -2]
+# target = 9
+# result = two_sum_indices(nums, target)
+
+# if result.empty?
+#   puts "No such pairs found."
+# else
+#   puts "Indices of the pairs: #{result}"
+# end
 
 
 
 #Explaination
 
-# The approach you're using, often called the "two-pointer technique" with a hash table, is already quite 
-# efficient with a time complexity of O(n) where n is the size of the array. It's one of the most 
+# The approach you're using, often called the "two-pointer technique" with a hash table, is already quite
+# efficient with a time complexity of O(n) where n is the size of the array. It's one of the most
 # optimal ways to solve this problem.
 
-# If you're looking for alternative methods, you could use nested loops, but the time complexity would be O(n^2), 
+# If you're looking for alternative methods, you could use nested loops, but the time complexity would be O(n^2),
 # which is less efficient. Here's an example of the nested loop approach:
 
 # # another solution
@@ -66,5 +89,5 @@ end
 #   puts "Indices of the pairs (bruteforce): #{result_bruteforce}"
 # end
 
-# However, as mentioned, the nested loop approach is less efficient, especially for large arrays. 
+# However, as mentioned, the nested loop approach is less efficient, especially for large arrays.
 # Stick with the original solution using a hash table, as it is more optimal.
