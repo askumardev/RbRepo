936dd7dabc90f160e364431d8f19c10b98440c26
askumardev <askumar.dev@outlook.com>
2026-05-17 22:37:13 +0530

    move zeros

---
+++ algo/move_zeros.rb
@@ -0,0 +1,33 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+# Example 1:
+# Input
+# nums = [0,1,0,3,12]
+
+# ruby algo/move_zeros.rb
+
+def move_zeroes(nums)
+  insert_pos = 0
+
+  # Move all non-zero elements to the front
+  nums.each do |num|
+    if num != 0
+      nums[insert_pos] = num
+      insert_pos += 1
+    end
+  end
+
+  # Fill remaining positions with 0
+  while insert_pos < nums.length
+    nums[insert_pos] = 0
+    insert_pos += 1
+  end
+
+  nums
+end
+
+# Example
+nums = [0, 1, 0, 3, 12]
+puts move_zeroes(nums).inspect
