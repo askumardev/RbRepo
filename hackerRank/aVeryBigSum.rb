6aa1cdf46569d11e5db7d134cdb249ecab402f83
askumardev <askumar.dev@outlook.com>
2026-05-27 19:24:52 +0530

    very big sum

---
+++ hackerRank/aVeryBigSum.rb
@@ -0,0 +1,17 @@
+def aVeryBigSum(arr)
+  arr.sum
+end
+
+# Input
+n = gets.to_i
+arr = gets.split.map(&:to_i)
+
+# Output
+puts aVeryBigSum(arr)
+
+
+# 5
+# 1000000001 1000000002 1000000003 1000000004 1000000005
+
+
+# ruby hackerRank/aVeryBigSum.rb
