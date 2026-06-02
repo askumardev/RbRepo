ca4c52f6067b13050b6e6eb880ab6fba217eab56
askumardev <askumar.dev@outlook.com>
2026-06-02 23:09:22 +0530

    update

--- interview/vaaree.rb
+++ interview/vaaree.rb
@@ -1,16 +1,16 @@
 # ruby interview/vaaree.rb
 
 def whitelist(str)
-  
+
   # Split the string into words
   words = str.split
-  
+
   # Define a regular expression to match words with only alphabetic characters
   whitelist_regex = /^[A-Za-z,]+$/
-  
+
   # Filter words using the regular expression
   valid_words = words.select { |word| word.match?(whitelist_regex) }
-  
+  p valid_words
   # Output the valid words
   puts "Valid words: #{valid_words.join(' ')}"
   puts "Count of valid words: #{valid_words.length}"
@@ -18,6 +18,6 @@ end
 
 str = "This is saturday13."
 whitelist(str)
-
+p "==================================="
 str = "Hello World!, N@mastey Satish. We are whitelisting !tems here..."
 whitelist(str)
