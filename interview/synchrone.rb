ff0b9820206c701d089878c62bc03adf9c437cfe
askumardev <askumar.dev@outlook.com>
2026-06-03 00:30:51 +0530

    longest_substring_without_repetition

--- interview/synchrone.rb
+++ interview/synchrone.rb
@@ -1,75 +1,46 @@
 # ruby interview/synchrone.rb
 
-def longest_substring_without_repeating(s)
-  n = s.length
-  max_length = 0
+def longest_substring_without_repetition(str)
   start = 0
-  last_seen = {}
-  longest_substring = ""
+  max_length = 0
+  longest = ""
+  hsh = {}
 
-  (0...n).each do |i|
-    p [i, s[i], start]
-    if last_seen.key?(s[i]) && last_seen[s[i]] >= start
-      p "=========first if======"
-      p last_seen.key?(s[i])
-      p last_seen[s[i]]
-      
-      start = last_seen[s[i]] + 1
-      p start
+  str.chars.each_with_index do |char, current|
+    puts "\nProcessing '#{char}' at index #{current}"
+    p hsh[char]
+    # Duplicate found inside current window
+    if hsh.key?(char) && hsh[char] >= start
+      puts "Duplicate '#{char}' found at index #{hsh[char]}"
+      start = hsh[char] + 1
+      puts "Moving start to #{start}"
     end
 
-    last_seen[s[i]] = i
-    p last_seen
-    current_substring = s[start..i]
-    p current_substring
-    if current_substring.length > longest_substring.length
-      p "-------second if-----------"
-      p current_substring
-      p longest_substring
-      longest_substring = current_substring
-      p longest_substring
+    # Store latest position
+    hsh[char] = current
+
+    current_length = current - start + 1
+
+    puts "Current Window: #{str[start..current]}"
+    puts "Current Length: #{current_length}"
+
+
+    # current valid substring is longer than any substring we've seen so far.
+    if current_length > max_length
+      max_length = current_length
+      longest = str[start..current]
+
+      puts "New Longest Substring: #{longest}"
+      puts "Max Length: #{max_length}"
     end
-    p "------end of iteration #{i}-----"
+
+    puts "Hash: #{hsh}"
   end
 
-  longest_substring
+  longest
 end
 
-# Example usage:
-string = "abcaaadefdgdhijkl"
-longest_substring = longest_substring_without_repeating(string)
-puts "Longest substring without repeating characters: #{longest_substring}"
+puts "\nResult:"
+puts longest_substring_without_repetition("abcaaadefdgdhijkl")
 
-
-
-# def longest_substrings_without_repeating(s)
-#   result = 0
-#   hash = {}
-#   j = 0
-#   substrings = []
-
-#   for i in 0...s.length
-#     if hash.has_key?(s[i])
-#       p hash
-#       p s[i]
-#       j = [hash[s[i]] + 1, j].max
-#       p j
-#     end
-#     current_length = i - j + 1
-#     if current_length > result
-#       result = current_length
-#       substrings = [s[j..i]]
-#     elsif current_length == result
-#       substrings << s[j..i]
-#     end
-#     hash[s[i]] = i
-#   end
-
-#   return result, substrings
-# end
-
-# # Example usage:
-# string = "abcaaadefdgdhijkl"
-# length, substrings = longest_substrings_without_repeating(string)
-# puts "Length of the longest substrings without repeating characters: #{length}"
-# puts "Longest substrings without repeating characters: #{substrings}"
+#puts longest_substringu("abcaaadefdgdhijkl")
