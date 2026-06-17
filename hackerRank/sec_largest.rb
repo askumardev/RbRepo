bf7a0d4d0fd3a1deac738422372396747c6bdd11
askumardev <askumar.dev@outlook.com>
2026-06-17 19:43:42 +0530

    2nd largest

---
+++ hackerRank/sec_largest.rb
@@ -0,0 +1,20 @@
+# ruby hackerRank/sec_largest.rb
+
+def second_largest(arr)
+  largest = second = -Float::INFINITY
+  p largest
+
+  arr.each do |n|
+    if n > largest
+      second = largest
+      largest = n
+    elsif n > second && n != largest
+      second = n
+    end
+  end
+
+  second == -Float::INFINITY ? nil : second
+end
+
+puts second_largest([10, 5, 8, 20, 15])  # => 15
+puts second_largest([5, 5, 5])           # => nil (no true second largest)
