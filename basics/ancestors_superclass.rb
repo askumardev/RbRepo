3c190af60c9194db65d0ee5fab04d842fd06fae1
askumardev <askumar.dev@outlook.com>
2026-07-14 05:06:43 +0000

    update

--- basics/ancestors_superclass.rb
+++ basics/ancestors_superclass.rb
@@ -1,15 +1,32 @@
 # ruby basics/ancestors_superclass.rb
 
-
-class Samp
+module SampMod
   def sample
-    puts "sample data"
+    puts "sample module data"
   end
 end
 
-s = Samp.new
+class SampCls
+  def sample
+    puts "sample class data"
+  end
+end
+
+s = SampCls.new
 s.sample
 p "--------------------"
-puts Samp.ancestors
+puts SampCls.ancestors
 p "--------------------"
 puts s.class.ancestors
+
+p "---------class superclass--------------"
+p SampCls.class
+p SampCls.class.superclass
+p SampCls.class.superclass.superclass
+p SampCls.class.superclass.superclass.superclass
+
+p "---------class superclass--------------"
+p SampMod.class
+p SampMod.class.superclass
+p SampMod.class.superclass.superclass
+p SampMod.class.superclass.superclass.superclass #nil
